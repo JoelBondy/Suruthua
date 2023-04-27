0bceeb481ea441887bb07386733509fee03fcd37
JoelBondy <joel-elchanan.bondy@student.uni-tuebingen.de>
2023-04-28 02:33:09 +0300

    füge skript für suchleiste hinzu

---
+++ metakram/searchbar.js
@@ -0,0 +1,104 @@
+const all_pages = {
+    "Amaka":"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Amaka/Amaka.html",
+    "Amaka (Regelwerk)":"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Amaka/Amaka_Regelwerk.html",
+    "Attribute":"https://joelbondy.github.io/Suruthua/Regelwerk/Charakterbogen/Attribute.html",
+    "Augling":"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Augling/Augling.html",
+    "Augling (Regelwerk)":"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Augling/Augling_Regelwerk.html",
+    'Avzitil':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Avzitil/Avzitil.html",
+    'Avztil (Regelwerk)':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Avzitil/Avzitil_Regelwerk.html",
+    'Berethao':"https://joelbondy.github.io/Suruthua/Umgebung/Karte/Kontinente/Berethao/Berethao.html",
+    'Blauer Traum':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Flora/Blauer_Traum/Blauer_Traum.html",
+    'Blib':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/Tiere/Blib/Blib.html",
+    'Blutborke':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Flora/Blutborke/Blutborke.html",
+    'Charakterbogen':"https://joelbondy.github.io/Suruthua/Regelwerk/Charakterbogen/Charakterbogen.html",
+    "Cul'thur":"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/MagischeKreaturen/VerfluchteWesen/Culthur/Culthur.html",
+    'Draske':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Draske/Draske.html",
+    'Draske (Regelwerk)':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Draske/Draske_Regelwerk.html",
+    'Elb':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Elb/Elb.html",
+    'Elb (Regelwerk)':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Elb/Elb_Regelwerk.html",
+    'Entstehung der Welt':"https://joelbondy.github.io/Suruthua/Kulturen/Religion/Entstehung_der_Welt.html",
+    'Flossenbeutler':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/Tiere/Flossenbeutler/Flossenbeutler.html",
+    'Frostwein':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Flora/Frostwein/Frostwein.html",
+    'Gebirge':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Geologie/Gebirge.html",
+    'Gegner':"https://joelbondy.github.io/Suruthua/Regelwerk/ListeAllerGegner.html",
+    'Geologie':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Geologie/Geologie.html",
+    'Gewässer':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Gewaesser/Gewaesser.html",
+    'Gloq':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/Tiere/Gloq/Gloq.html",
+    'Golem':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/MagischeKreaturen/Golem/Golem.html",
+    'Große Persönlichkeiten':"https://joelbondy.github.io/Suruthua/Kulturen/GrossePersoenlichkeiten/GrossePersoenlichkeiten.html",
+    'Guqual':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Guqual/Guqual.html",
+    'Guqual (Regelwerk)':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Guqual/Guqual_Regelwerk.html",
+    'Ideen':"https://joelbondy.github.io/Suruthua/metakram/Ideen.html",
+    'Ideen (Tiere)':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/Tiere/Ideen.html",
+    'Inseln':"https://joelbondy.github.io/Suruthua/Umgebung/Karte/Kontinente/Inseln/Inseln.html",
+    'Kampf':"https://joelbondy.github.io/Suruthua/Regelwerk/Kampf/Kampf.html",
+    'Karte':"https://joelbondy.github.io/Suruthua/Umgebung/Karte/Karte.html",
+    "Keil'O Renn":"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/Tiere/KeilO_Renn/KeilO_Renn.html",
+    'Kiesenriller-Maus':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/Tiere/KiesenrillerMaus/KiesenrillerMaus.html",
+    'Knochendorn':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Flora/Knochendorn/Knochendorn.html",
+    'Lachratte':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/Tiere/Lachratte/Lachratte.html",
+    'Loromoth':"https://joelbondy.github.io/Suruthua/Umgebung/Karte/Kontinente/Loromoth/Loromoth.html",
+    'Magie':"https://joelbondy.github.io/Suruthua/Regelwerk/Magie.html",
+    'Mensch':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Mensch/Mensch/Mensch.html",
+    'Namen':"https://joelbondy.github.io/Suruthua/metakram/Namen.html",
+    'Naturphänomene':"https://joelbondy.github.io/Suruthua/Umgebung/Astronomie/Sonnensystem/Naturph%C3%A4nomene.html",
+    'Parasitkäfer':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/Tiere/Parasitkaefer/Parasitkaefer.html",
+    'Pilze':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Flora/Pilze/Pilze.html",
+    'Quork':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Quork/Quork.html",
+    'Quork (Regelwerk)':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Quork/Quork_Regelwerk.html",
+    'Regeln':"https://joelbondy.github.io/Suruthua/Regelwerk/Regeln.html",
+    "Religionen und Götter":"https://joelbondy.github.io/Suruthua/Kulturen/Religion/Goetter_Religion.html",
+    'Religion (Regelwerk)':"https://joelbondy.github.io/Suruthua/Kulturen/Religion/Goetter_Religion_Regelwerk.html",
+    'Sauerbaum':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Flora/Sauerbaum/Sauerbaum.html",
+    'Schreiender Triesel':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Flora/Schreiender_Triesel/Schreiender_Triesel.html",
+    'Sonnensytem':"https://joelbondy.github.io/Suruthua/Umgebung/Astronomie/Sonnensystem/Sonnensystem.html",
+    'Triesel':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/Tiere/Triesel/Triesel.html",
+    'Trollbrücke':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/MagischeKreaturen/Trollbruecke/Trollbruecke.html",
+    'Tropfbeutel':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Flora/Tropfbeutel/Tropfbeutel.html",
+    'Tropfbeutler':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/Tiere/Tropfbeutler/Tropfbeutler.html",
+    'Übernehmen (Natur)':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Uebernehmen.html",
+    'Vadonier':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Vadonier/Vadonier.html",
+    'Vadonier (Regelwerk)':"https://joelbondy.github.io/Suruthua/Kulturen/Rassen/Vadonier/Vadonier_Regelwerk.html",
+    'Verfluchte Wesen':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/MagischeKreaturen/VerfluchteWesen/Verfluchte_Wesen.html",
+    'Verfluchter Quork':"https://joelbondy.github.io/Suruthua/Umgebung/Natur/Fauna/MagischeKreaturen/VerfluchteWesen/VQuork/VQuork.html",
+    'Waffenklassen':"https://joelbondy.github.io/Suruthua/Regelwerk/Kampf/Waffenklassen.html",
+    'Wichtige Daten':"https://joelbondy.github.io/Suruthua/Kulturen/Zeitrechnung/WichtigeDaten.html",
+    'Wirinima':"https://joelbondy.github.io/Suruthua/Umgebung/Karte/Kontinente/Wirinima/Wirinima.html",
+    'Währung':"https://joelbondy.github.io/Suruthua/Kulturen/Waehrung.html",
+    'Xaraton':"https://joelbondy.github.io/Suruthua/Umgebung/Karte/Kontinente/Xaraton/Xaraton.html",
+    'Zeitrechnung':"https://joelbondy.github.io/Suruthua/Kulturen/Zeitrechnung/Zeitrechnung.html",
+};
+
+
+    
+let results = document.getElementById("results");
+Object.entries(all_pages).forEach(([key,value])=>{
+    let a = document.createElement("a");
+    a.innerText = key;
+    a.href = value;
+    a.classList.add("resultItem");
+    results.appendChild(a);
+});
+
+function search() {
+    let resultsContainer = document.getElementById("results");
+    let resultItems = document.getElementsByClassName("resultItem");
+
+    let input = document.getElementById("searchbar").value
+    input=input.toLowerCase();
+
+    let noResults = true;
+    for (i=0;i<resultItems.length;i++) {
+        if (!resultItems[i].innerHTML.toLowerCase().includes(input) || input === "") {
+            resultItems[i].style.display="none";
+            continue;
+        }
+        else {
+            resultItems[i].style.display="flex";
+            noResults=false;
+        }
+    }
+
+    resultsContainer.style.display = noResults ? "none":"block";
+};
+
